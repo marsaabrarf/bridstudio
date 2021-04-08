8bfdea06a5aed0ce8542c82f3ebcf4297255bec4
Marsa Abrar F <marsaabrarf@gmail.com>
2021-04-09 00:52:26 +0700

    Added responsive nav links for mobile and desktop

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,3 +2,13 @@ $(document).scroll(function () {
   var $nav = $(".appBar");
   $nav.toggleClass("scrolled", $(this).scrollTop() > $(window).height());
 });
+
+$(".hamburger").click(function () {
+  $(".ul-container").toggleClass("ul-container-active");
+  $(this).toggleClass("close");
+});
+
+$(".nav-link").click(function () {
+  $(".ul-container").toggleClass("ul-container-active");
+  $(".hamburger").toggleClass("close");
+});
